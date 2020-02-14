e1eae0575089f0928be04c6e8ba933a537e031c7
Taina Damasceno <damasceno.tad@gmail.com>
2020-02-14 17:03:38 +0100

    Added body position evaluation for any given time

--- circular_simple.py
+++ circular_simple.py
@@ -18,49 +18,70 @@ class Star:
         self.ypos = ypos
     
     def pos(self):
-        return self.xpos, self.ypos
+        return [self.xpos, self.ypos]
     
 spica = Star(1, 0)
-#print(spica.pos())
+print(spica.pos())
 
 
 #Create a body orbiting the star
 
 class Body:
     
-    def __init__(self, center_xpos, center_ypos, radius, perigee_argument, period):
-        self.center_xpos = center_xpos
-        self.center_ypos = center_ypos
+    def __init__(self, center_pos, radius, perigee_argument, \
+                 period, time_on_perigee):
+        
+        
+        self.center_xpos = center_pos[0]
+        self.center_ypos = center_pos[1]
         self.radius = radius
-        self.perigee_argument = perigee_argument
-        self.period = period
-        self.orbit = plt.Circle((center_xpos, center_ypos), radius, \
-                                fill = False)
+        self.perigee_argument = perigee_argument #in DEGREES
+        self.period = period  
+        self.time_on_perigee= time_on_perigee #must use same units as period
         
+        self.orbit = plt.Circle((self.center_xpos, self.center_ypos), \
+                                self.radius, fill = False)
         
-    def pos_init(self):
+            
+    def pos_angle(self, angle_deg):
         xpos = self.center_xpos +   \
-               self.radius*math.cos(math.radians(self.perigee_argument))
-               
+               self.radius*math.cos(math.radians(angle_deg))
         ypos = self.center_ypos +   \
-               self.radius*math.sin(math.radians(self.perigee_argument))
-        return xpos, ypos
+               self.radius*math.sin(math.radians(angle_deg))
+        return [xpos, ypos]
+
+    def pos_init(self):
+        [xpos, ypos] = self.pos_angle(self.perigee_argument)
+        return [xpos, ypos]
     
-    
-tatooine = Body(*spica.pos(), 0.3, 0, 1)
+    def pos_time(self, time):
+        run_time = time - self.time_on_perigee
+        angle_time_deg = math.degrees(2*math.pi*(run_time / (self.period)))
+        [xpos_time, ypos_time] = self.pos_angle(angle_time_deg)
+        return [xpos_time, ypos_time]
+        
+
+# Creating planet: Tatooine, centered on Spica, radius 0.3
+                 # perigee at 0 deg, period of 1 "year"
+                 # passing its perigee at time 0 "year"
+                 
+tatooine = Body(spica.pos(), 0.3, 0, 1, 0)
 #print(tatooine.pos_init())
 
 ## Parte de plot - Roncha pode brincar depois
-plt.plot(*spica.pos(), 'xr')
+'''
+plt.plot(spica.xpos, spica.ypos, 'xr')
 plt.grid(True)
 
 ax = plt.gca()
 ax.add_patch(tatooine.orbit)
 plt.axis('scaled')
 plt.show()
+'''
 
-
-
-#Circular orbit
+##### Circular orbit
+# First: evaluate the body position in a given time
+# DONE basic calling body.pos_time(time) will return list with [xpos, ypos]
 
 #Plot the path between 2 dates
+# TO DO
